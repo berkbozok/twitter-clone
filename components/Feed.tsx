022112d2a97f7bcc5acfcfc38541b1b061e8d465
Berk Bozok <berk_bozok@live.com>
2022-07-20 19:52:52 -0400

    center added

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,10 +1,11 @@
 import { RefreshIcon } from '@heroicons/react/outline'
 import React from 'react'
+import TweetBox from './TweetBox'
 
 function Feed() {
   return (
 
-    <div className='col-span-7 lg:col-span-5'>
+    <div className='col-span-7 lg:col-span-5 border-x'>
         <div className='flex items-center justify-between '>
         
         <h1 className='p-5 pb-0 text-xl font-bold'>Home</h1>
@@ -14,6 +15,12 @@ function Feed() {
 
 
         </div>
+
+            <div>
+                    <TweetBox/>
+
+            </div>
+
     </div>
   )
 }
--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -14,7 +14,7 @@ function SidebarRow({Icon,title}:Props) {
     <div className="flex max-w-fit items-center space-x-2 px-4 py-3 rounded-full  transition-all duration:200 hover:bg-gray-100 group">
         
         <Icon className='h-6 w-6'/>
-        <p className="group-hover:text-twitter hidden md:inline-flex">
+        <p className="group-hover:text-twitter hidden md:inline-flex text-base font-light lg:text-xl">
             {title}
         </p>
 
