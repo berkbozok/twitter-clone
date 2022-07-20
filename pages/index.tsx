83e31956fd6454921c160e0fe00a476e1c3545d6
Berk Bozok <berk_bozok@live.com>
2022-07-20 01:56:15 -0400

    icons added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Sidebar from '../components/Sidebar'
 
 const Home: NextPage = () => {
   return (
@@ -13,7 +14,7 @@ const Home: NextPage = () => {
      
     <main>
 
-      {/* Sidebar */}
+      <Sidebar/>
       
       {/* Feed */}
 
